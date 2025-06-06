9478ada7793331d1a88f2a4b06a1aadec4a6aadb
IanSC23 <saci9502@hotmail.com>
2025-06-06 12:37:06 -0600

    Added checklist with scatter graphs

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ import plotly.express as px
 car_data = pd.read_csv('/Users/iansc/Documentos/vehicles_env/vehicles_us.csv')
 df = pd.DataFrame(car_data)
 
-st.header('Vehículos en venta')
+st.title('Vehículos en venta')
 
-hist_check = st.checkbox('Crear Histograma')
-scat_check = st.checkbox('Crear un gráfico de dispersión')
+st.header('Información del histograma mostrando el odómetro de los vehículos')
 
-if hist_check:
+# Crear botón para histograma
+hist_button = st.button('Crear Histograma')
+
+if hist_button:
     # escribe un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -23,13 +25,32 @@ if hist_check:
     # Mostrar un gáfico Plotly interactivo
     st.plotly_chart(hist_fig, use_container_width=True)
 
-if scat_check:
+st.divider()
+
+st.header('Gráficos de dispersión en base a diferentes comparativas de datos')
+
+
+price_year_check = st.checkbox('Compara precio con año del modelo')
+price_odom_check = st.checkbox('Compara precio con millaje recorrido')
+
+if price_year_check:
     # escribe un mensaje
     st.write(
         'Creación de un gráfico de dispersión del precio en relación al año del modelo')
 
     # crea histograma
-    scat_fig = px.scatter(car_data, x="model_year", y="price")
+    scat_pr_ye = px.scatter(car_data, x="model_year", y="price")
 
     # Mostrar un gáfico Plotly interactivo
-    st.plotly_chart(scat_fig, use_container_width=True)
+    st.plotly_chart(scat_pr_ye, use_container_width=True)
+
+if price_odom_check:
+    # escribe un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión del precio en relación al millaje recorrido del vehículo')
+
+    # crea histograma
+    scat_pr_od = px.scatter(car_data, x="odometer", y="price")
+
+    # Mostrar un gáfico Plotly interactivo
+    st.plotly_chart(scat_pr_od, use_container_width=True)
